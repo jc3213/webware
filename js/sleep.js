f3087a2d0bddf7238a9544382ed43218e32f6655
jc3213 <jc3213@users.noreply.github.com>
2025-10-18 22:46:18 +0800

    Update and rename timeout.js to sleep.js

---
+++ js/sleep.js
@@ -0,0 +1,1 @@
+const sleep = (e) => new Promise((r) => setTimeout(r, e));
--- js/timeout.js
+++
@@ -1,7 +0,0 @@
-function PromiseTimeout(n) {
-    return new Promise((resolve, reject) => {
-        Number.isFinite(n) && n > 0 
-            ? setTimeout(resolve, n * 1000)
-            : reject(new TypeError("parameter 1 must be a non-negative number!"));
-    });
-}
